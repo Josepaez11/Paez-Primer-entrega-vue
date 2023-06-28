d67cdf0b83f90574826ea2e67e7ffb9d734b6f2d
Jose Paez <josefinapaezdevalle@gmail.com>
2023-06-28 10:56:51 -0300

    Segunda-Entrega

--- src/views/InfoView.vue
+++ src/views/InfoView.vue
@@ -1,15 +1,15 @@
 <template>
-  <div>
-    <nav-component />
-    <info-component />
-  </div>
-</template>
-
-<script>
-import InfoComponent from "@/components/InfoComponent.vue";
-import NavComponent from "@/components/NavComponent.vue";
-export default {
-  components: { InfoComponent, NavComponent },
-  name: "InfoView",
-};
-</script>
+    <div>
+      <nav-component />
+      <info-component />
+    </div>
+  </template>
+  
+  <script>
+  import InfoComponent from "@/components/InfoComponent.vue";
+  import NavComponent from "@/components/NavComponent.vue";
+  export default {
+    components: { InfoComponent, NavComponent },
+    name: "InfoView",
+  };
+  </script>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,15 +1,15 @@
 <template>
-  <div>
-    <nav-component />
-    <login-component />
-  </div>
-</template>
-
-<script>
-import LoginComponent from "@/components/LoginComponent.vue";
-import NavComponent from "@/components/NavComponent.vue";
-export default {
-  components: { LoginComponent, NavComponent },
-  name: "LoginView",
-};
-</script>
+    <div>
+      <nav-component />
+      <login-component />
+    </div>
+  </template>
+  
+  <script>
+  import LoginComponent from "@/components/LoginComponent.vue";
+  import NavComponent from "@/components/NavComponent.vue";
+  export default {
+    components: { LoginComponent, NavComponent },
+    name: "LoginView",
+  };
+  </script>
