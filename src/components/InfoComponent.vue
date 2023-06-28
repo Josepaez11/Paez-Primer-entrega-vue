b9a792e237259221f65283500910a9b6c22a284f
Jose Paez <josefinapaezdevalle@gmail.com>
2023-06-28 10:49:55 -0300

    segunda-entrega

--- src/components/InfoComponent.vue
+++ src/components/InfoComponent.vue
@@ -1,52 +1,50 @@
 <template>
-  <!-- No puedo obtener los datos de la API para mostrarlos segun el ID de cada producto -->
-  
-  <div class="my-10">
-    <h2 class="text-xl font-semibold text-center">Información del producto</h2>
-    <div
-      class="w-full max-w-3xl min-h-full mx-auto bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700"
-    >
-      <img class="p-8 rounded-t-lg" src="imagen" alt="nombre" />
-      <div class="px-5 pb-5">
-        <h5
-          class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
-        >
-          nombre
-        </h5>
-        <div>
-          <p
-            class="text-sm font-semibold tracking-tight text-gray-900 dark:text-white"
+    <div class="my-10">
+      <h2 class="text-xl font-semibold text-center">Información del producto</h2>
+      <div
+        class="w-full max-w-3xl min-h-full mx-auto bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700"
+      >
+        <img class="p-8 rounded-t-lg" src="imagen" alt="nombre" />
+        <div class="px-5 pb-5">
+          <h5
+            class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
           >
-            Info del producto
-          </p>
-          <p
-            class="text-sm font-semibold tracking-tight text-gray-900 dark:text-white"
-          >
-            info
-          </p>
-        </div>
-        <div class="flex items-center justify-between">
-          <span class="text-2xl font-bold text-gray-900 dark:text-white"
-            >$ precio</span
-          >
-          <a
-            href="#"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >Añadir al Carrito</a
-          >
+            nombre
+          </h5>
+          <div>
+            <p
+              class="text-sm font-semibold tracking-tight text-gray-900 dark:text-white"
+            >
+              Info del producto
+            </p>
+            <p
+              class="text-sm font-semibold tracking-tight text-gray-900 dark:text-white"
+            >
+              info
+            </p>
+          </div>
+          <div class="flex items-center justify-between">
+            <span class="text-2xl font-bold text-gray-900 dark:text-white"
+              >$ precio</span
+            >
+            <a
+              href="#"
+              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >Añadir al Carrito</a
+            >
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</template>
-
-<script>
-export default {
-  name: "InfoComponent",
-
-  data() {
-    return {};
-  },
-  methods: {},
-};
-</script>
+  </template>
+  
+  <script>
+  export default {
+    name: "InfoComponent",
+  
+    data() {
+      return {};
+    },
+    methods: {},
+  };
+  </script>
--- src/components/ListComponent.vue
+++ src/components/ListComponent.vue
@@ -1,38 +1,38 @@
 <template>
-  <div class="my-10">
-    <h1 class="my-6 text-4xl font-semibold text-center">Productos</h1>
-    <div class="flex flex-wrap justify-center gap-6 mx-6">
-      <CardComponent
-        v-for="(producto, i) of productos"
-        :key="i"
-        :productos="producto"
-      />
+    <div class="my-10">
+      <h1 class="my-6 text-4xl font-semibold text-center">Productos</h1>
+      <div class="flex flex-wrap justify-center gap-6 mx-6">
+        <CardComponent
+          v-for="(producto, i) of productos"
+          :key="i"
+          :productos="producto"
+        />
+      </div>
     </div>
-  </div>
-</template>
-
-<script>
-import CardComponent from "./CardComponent.vue";
-import axios from "axios";
-// import InfoComponent from "./InfoComponent.vue";
-
-export default {
-  components: { CardComponent },
-  name: "ListComponent",
-  data() {
-    return {
-      productos: [],
-    };
-  },
-  created() {
-    // LLAMADA A API DE PRODUCTOS AXIOS
-    const GETURL = "https://63a23154a543280f776af0c4.mockapi.io/productos";
-    axios
-      .get(GETURL)
-      .then((response) => {
-        this.productos = response.data;
-      })
-      .catch((err) => console.log(err));
-  },
-};
-</script>
+  </template>
+  
+  <script>
+  import CardComponent from "./CardComponent.vue";
+  import axios from "axios";
+  // import InfoComponent from "./InfoComponent.vue";
+  
+  export default {
+    components: { CardComponent },
+    name: "ListComponent",
+    data() {
+      return {
+        productos: [],
+      };
+    },
+    created() {
+      // LLAMADA A API DE PRODUCTOS AXIOS
+      const GETURL = "https://63a23154a543280f776af0c4.mockapi.io/productos";
+      axios
+        .get(GETURL)
+        .then((response) => {
+          this.productos = response.data;
+        })
+        .catch((err) => console.log(err));
+    },
+  };
+  </script>
